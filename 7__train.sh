c02ba675f211c6a2d25168fed98d27bb4761c703
sw.han <sw.han@wego-robotics.com>
2025-10-16 10:30:51 +0900

    change train script at server

--- 7__train.sh
+++ 7__train.sh
@@ -7,15 +7,16 @@ echo $HF_USER
 python ./src/lerobot/scripts/train.py \
 --policy.device=cuda  \
 --policy.type=smolvla   \
---policy.repo_id=piper_smolvla  \
---policy.push_to_hub=false \
---dataset.repo_id=wego-hansu/piper_pick_yellow_car_033   \
+--policy.repo_id=piper_teleop_smolvla  \
+--policy.push_to_hub=true \
+--dataset.repo_id=wego-hansu/piper_tele_pick_cars_033_B   \
 --dataset.video_backend=pyav   \
 --batch_size=64   \
---steps=25000   \
+--steps=200000   \
 --eval_freq=0   \
---output_dir=outputs/train/piper_smolvla_pick_yellow_cars_033   \
---job_name=piper_smolvla_yellow_car   \
+--save_freq=5000 \
+--output_dir=outputs/train/piper_smolvla_teleop_033_B   \
+--job_name=piper_smolvla_cars   \
 --wandb.enable=false  \
---resume=true  \
---config_path=outputs/train/piper_smolvla_pick_yellow_cars_033/checkpoints/020000/pretrained_model/train_config.json\
+# --resume=true  \
+# --config_path=outputs/train/piper_smolvla_pick_yellow_cars_033/checkpoints/last/pretrained_model/train_config.json\
